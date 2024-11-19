dc4cb0f0837d2b275d79df63c7c97284510de699
Maria Florina Iancău <iancau.maria.imf@gmail.com>
2024-11-19 21:23:20 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IancauMariaLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
