e2a4fe292093fd68c1791e157f2f9d9ee9dfc9d1
iancaumaria <iancau.maria.imf@gmail.com>
2024-12-13 21:56:48 +0200

    da

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace IancauMariaLab7
+﻿using System;
+
+using System.IO;
+using IancauMariaLab7.Data;
+
+namespace IancauMariaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
